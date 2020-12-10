5ed948c3ac29c6016a00335aaa2e83c105139886
Nghia Nguyen <naguyen22@gmail.com>
2020-12-10 17:59:21 -0500

    refactor JS

--- js/app.js
+++ js/app.js
@@ -17,8 +17,8 @@
  * Define Global Variables
  *
  */
-const sections = document.querySelectorAll("section");
-const navBar = document.querySelector("#navbar__list");
+const sections = document.querySelectorAll(`section`);
+const navBar = document.querySelector(`#navbar__list`);
 const df = document.createDocumentFragment();
 
 /**
@@ -26,7 +26,9 @@ const df = document.createDocumentFragment();
  * Start Helper Functions
  *
  */
-
+function createElement(ele) {
+  return document.createElement(ele);
+}
 
 /**
  * End Helper Functions
@@ -37,61 +39,64 @@ const df = document.createDocumentFragment();
 // build the nav
 
 function buildNav() {
+
   // create nav link for each section
   sections.forEach((section) => {
     // create <li>
-    const liTag = document.createElement("li");
+    const liTag = createElement(`li`);
 
     // create <a> and insert data
-    const aTag = document.createElement("a");
-    aTag.setAttribute("href", `#${section.id}`);
+    const aTag = createElement(`a`);
+    aTag.setAttribute(`href`, `#${section.id}`);
     aTag.textContent = section.dataset.nav;
 
     // appending process
     liTag.appendChild(aTag);
     df.appendChild(liTag);
     navBar.appendChild(df);
-  });
+  })
+
 }
-
-
 
 // Add class 'active' to section when near top of viewport
 
-  // 1. Check if section intersects with viewport using the Intersection Observer API
+// Check if section intersects with viewport using the Intersection Observer API
+
+function setActiveSection() {
 
   // Creating an intersection observer and options
   let options = {
     root: null,
-    rootMargin: "0px",
+    rootMargin: `0px`,
     threshold: 1,
   };
 
-  let observer = new IntersectionObserver(activeState, options);
+  let observer = new IntersectionObserver(callback, options);
 
   // Adding each section to be observed
   sections.forEach((section) => observer.observe(section));
 
   // cb that will be triggered
-  function activeState(entries) {
+  function callback(entries) {
     entries.forEach((entry) => {
-      console.log(entry.intersectionRatio);
       if (entry.intersectionRatio >= 1) {
-        entry.target.classList.toggle("your-active-class");
+        entry.target.classList.toggle(`your-active-class`);
       } else {
-        entry.target.classList.remove("your-active-class");
+        entry.target.classList.remove(`your-active-class`);
       }
     });
   }
 
-// Scroll to anchor ID using scrollTO event
-navBar.addEventListener('click', evt => {
+}
 
+// Scroll to anchor ID using scrollTO event
+
+function scrollToSection(evt) {
   // prevents default jump to section
   evt.preventDefault();
 
   // extract value from href attr
-  const id = evt.target.getAttribute('href');
+  const id = evt.target.getAttribute(`href`);
 
   // ref that specific section
   const section = document.querySelector(`${id}`)
@@ -99,10 +104,9 @@ navBar.addEventListener('click', evt => {
   // scroll to section
   window.scrollTo({
     top: section.offsetTop,
-    behavior: 'smooth',
+    behavior: `smooth`,
   })
-
-})
+};
 
 /**
  * End Main Functions
@@ -114,5 +118,7 @@ navBar.addEventListener('click', evt => {
 buildNav();
 
 // Scroll to section on link click
+navBar.addEventListener(`click`, scrollToSection);
 
 // Set sections as active
+setActiveSection();
